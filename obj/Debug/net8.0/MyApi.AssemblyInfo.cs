1e28f3d162e7b5f28dc07a5dd98226ea2df43b75
203mToan <2155010229@kientruchanoi.edu.vn>
2025-12-04 18:29:16 +0700

    your message

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082aae952da886e21454137116b049937495fc3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
