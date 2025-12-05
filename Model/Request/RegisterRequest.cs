ab0c366f5cde10e67d2272d569f18dba6ee91bdf
203mToan <2155010229@kientruchanoi.edu.vn>
2025-12-05 09:45:31 +0700

    register

---
+++ Model/Request/RegisterRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace MyApi.Model.Request
+{
+    public class RegisterRequest
+    {
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+
+        public string NationalId { get; set; } = null!;
+
+        // User sẽ nhập Password → backend sẽ Hash
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Model/Response/RegisterResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApi.Model.Response
+{
+    public class RegisterResponse
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+    }
+}
