11ede0da077c6c9fb108a0322eaf4532b5840166
203mToan <2155010229@kientruchanoi.edu.vn>
2025-12-15 22:44:40 +0700

    Update feature X

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda92bd3f50c17df09261ce4402f2c68177704cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0662b70ba8d18e5b516b5cf40ac4e4af48fe840")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
