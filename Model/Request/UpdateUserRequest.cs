1c498f7e5cecd401dff2ae0d069d4bfda524e9b2
203mToan <2155010229@kientruchanoi.edu.vn>
2026-01-07 01:07:14 +0700

    linh tinh

---
+++ Model/Request/UpdateUserRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApi.Model.Request
+{
+    public class UpdateUserRequest
+    {
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+    }
+}
--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8d021a3b416551f68f3c7319995c608de520f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a8ed16a4ebbbd2c2b045cce13b22b493f0882b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
